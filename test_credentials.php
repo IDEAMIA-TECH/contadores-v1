c8c3b872dbad01a76a8397ca5bdb1264e9df0e62
IDEAMIA-TECH <155779384+IDEAMIA-TECH@users.noreply.github.com>
2025-03-20 16:51:24 -0600

    Update test_credentials.php

--- test_credentials.php
+++ test_credentials.php
@@ -1,54 +1,78 @@
-<?php declare(strict_types=1);
+<?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+declare(strict_types=1);
 
-use PhpCfdi\Credentials\Credential;
-use PhpCfdi\SatWsDescargaMasiva\Service;
-use PhpCfdi\SatWsDescargaMasiva\WebClient\GuzzleWebClient;
-use PhpCfdi\SatWsDescargaMasiva\RequestBuilder\FielRequestBuilder\Fiel;
-use PhpCfdi\SatWsDescargaMasiva\RequestBuilder\FielRequestBuilder\FielRequestBuilder;
+namespace PhpCfdi\SatWsDescargaMasiva\Tests\Integration;
 
+use LogicException;
+use PhpCfdi\SatWsDescargaMasiva\Services\Query\QueryParameters;
+use PhpCfdi\SatWsDescargaMasiva\Shared\ComplementoRetenciones;
+use PhpCfdi\SatWsDescargaMasiva\Shared\DateTimePeriod;
+use PhpCfdi\SatWsDescargaMasiva\Shared\DocumentStatus;
+use PhpCfdi\SatWsDescargaMasiva\Shared\DownloadType;
+use PhpCfdi\SatWsDescargaMasiva\Shared\RequestType;
+use PhpCfdi\SatWsDescargaMasiva\Shared\RfcMatch;
+use PhpCfdi\SatWsDescargaMasiva\Shared\RfcOnBehalf;
+use PhpCfdi\SatWsDescargaMasiva\Shared\ServiceEndpoints;
+use PhpCfdi\SatWsDescargaMasiva\Shared\ServiceType;
+use PhpCfdi\SatWsDescargaMasiva\Shared\Uuid;
 
-$cerFile = __DIR__ . '/uploads/sat/sat_cer_67db47408517a.cer';
-$pemKeyFile = __DIR__ . '/uploads/sat/sat_key_67db4740851a2.key';
-$passPhrase = 'Japc20078'; // contraseña para abrir la llave privada
+/**
+ * @todo Parameter ComplementoRetenciones is failing, enable when SAT is working
+ */
+final class ConsumeRetencionesServicesUsingFakeFielTest extends ConsumeServiceTestCase
+{
+    protected function getServiceEndpoints(): ServiceEndpoints
+    {
+        return ServiceEndpoints::retenciones();
+    }
 
-$fiel = PhpCfdi\Credentials\Credential::openFiles($cerFile, $pemKeyFile, $passPhrase);
+    public function testQueryChangeFilters(): void
+    {
+        $service = $this->createService();
 
-$sourceString = 'texto a firmar';
-// alias de privateKey/sign/verify
-$signature = $fiel->sign($sourceString);
-echo "</br>Firma: </br>" . base64_encode($signature) . PHP_EOL;
+        $parameters = QueryParameters::create()
+            ->withPeriod(DateTimePeriod::createFromValues('2019-01-01 00:00:00', '2019-01-01 00:04:00'))
+            ->withDownloadType(DownloadType::received())
+            ->withRequestType(RequestType::xml())
+            //->withComplement(ComplementoRetenciones::undefined())
+            //->withDocumentStatus(DocumentStatus::active())
+            //->withRfcOnBehalf(RfcOnBehalf::create('XXX01010199A'))
+            //->withRfcMatch(RfcMatch::create('AAA010101AAA'))
+        ;
 
-// alias de certificado/publicKey/verify
-$verify = $fiel->verify($sourceString, $signature);
-var_dump($verify); // bool(true)
+        $result = $service->query($parameters);
+        $this->assertSame(
+            305,
+            $result->getStatus()->getCode(),
+            'Expected to receive a 305 - Certificado Inválido from SAT since FIEL is for testing'
+        );
+    }
 
-// objeto certificado
-$certificado = $fiel->certificate();
-echo "</br>RFC: </br>" . $certificado->rfc() . PHP_EOL; // el RFC del certificado
-echo "</br>Nombre:  </br>" . $certificado->legalName() . PHP_EOL; // el nombre del propietario del certificado
-echo "</br>Sucursal: </br>" . $certificado->branchName() . PHP_EOL; // el nombre de la sucursal (en CSD, en FIEL está vacía)
-echo "</br>Serial: </br>" . $certificado->serialNumber()->bytes() . PHP_EOL; // número de serie del certificado
+    public function testQueryByUuid(): void
+    {
+        $service = $this->createService();
 
+        $parameters = QueryParameters::create()
+            ->withUuid(Uuid::create('96623061-61fe-49de-b298-c7156476aa8b'))
+        ;
 
-$fiel = Fiel::create(
-    file_get_contents(__DIR__ . '/uploads/sat/sat_cer_67db47408517a.cer'),
-    file_get_contents(__DIR__ . '/uploads/sat/sat_key_67db4740851a2.key'),
-    'Japc20078'
-);
+        $result = $service->query($parameters);
+        $this->assertSame(
+            305,
+            $result->getStatus()->getCode(),
+            'Expected to receive a 305 - Certificado Inválido from SAT since FIEL is for testing'
+        );
+    }
 
-// verificar que la FIEL sea válida (no sea CSD y sea vigente acorde a la fecha del sistema)
-if (! $fiel->isValid()) {
-    return;
+    public function testServiceEndpointsDifferentThanQueryEndpointsThrowsError(): void
+    {
+        $service = $this->createService();
+
+        $otherServiceType = ServiceType::cfdi();
+        $parameters = QueryParameters::create()->withServiceType($otherServiceType);
+
+        $this->expectException(LogicException::class);
+        $service->query($parameters);
+    }
 }
-
-// creación del web client basado en Guzzle que implementa WebClientInterface
-// para usarlo necesitas instalar guzzlehttp/guzzle, pues no es una dependencia directa
-$webClient = new GuzzleWebClient();
-
-// creación del objeto encargado de crear las solicitudes firmadas usando una FIEL
-$requestBuilder = new FielRequestBuilder($fiel);
-
-// Creación del servicio
-$service = new Service($requestBuilder, $webClient);
